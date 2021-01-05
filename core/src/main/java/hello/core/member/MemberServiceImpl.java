25818a2877ba59fa0cfe7012fec9c150d08a2a01
kyumin <cgm97@naver.com>
2021-01-05 19:55:59 +0900

    ConfigSetting(객체생성,선언_class)

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // Interface 에서만 의존한다 - > 누군가 구현과 생성을 주입해줘야한다.(Config 사용)
+    private final MemberRepository memberRepository;
+
+    // 생성자 주입 -> AppConfig
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,22 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // Interface 에서만 의존한다 - > 누군가 구현과 생성을 주입해줘야한다.(Config 사용)
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 생성자 주입 -> AppConfig
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
