e25abb24e937edb651449235c6587aaa9765010e
kyumin <cgm97@naver.com>
2021-01-09 17:22:28 +0900

    @Configuration을 이용하여 싱글톤 적용 설정하지 않을시 싱클톤을 적용해주지않는다.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -3,21 +3,28 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// @Configuration ->스프링이 이것을 사용하여 싱글톤을 적용 시켜준다
 @Configuration
 public class AppConfig {
+
+    // @Autowired MemberRepository memberRepository;
 
     @Bean
     public MemberService memberService(){
         // 생성자 주입(연결)
         return new MemberServiceImpl(memberRepository());
+        // @Autowired을 이용해도 싱글톤 적용 할수있다.
+        // return new MemberServiceImpl(memberRepository);
     }
     @Bean
     public OrderService orderService(){
