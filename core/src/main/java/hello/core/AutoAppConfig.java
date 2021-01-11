f38ec5e5914ff032c9015bf90bf0a10bf9f60719
kyumin <cgm97@naver.com>
2021-01-11 20:09:27 +0900

    @ComponentScan,@Component,@AutoWired 의 사용

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // 컴포넌트 스캔 대상에서 제외(@Configuration)
+        // 기존 코드를 유지하기 위함 -> 실무에선 많이 사용하진 않는다.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
